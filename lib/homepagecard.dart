9ae615b543c20b0db191859c6e8a72a6dfcc7afa
NaveenSingh <naveensingh.mtp@gmail.com>
2021-10-30 00:31:05 +0530

    Text FontSize in HomePage

--- lib/homepagecard.dart
+++ lib/homepagecard.dart
@@ -15,13 +15,47 @@ class HomePageBottomCard extends StatelessWidget {
           color: Colors.white,
           borderRadius: BorderRadius.circular(30)
         ),
-        padding: const EdgeInsets.only(left:50, top:50),
-        child: const Text('Meet new era of',
-        style: TextStyle(
-          fontSize: 20,
-          color: Colors.pink
-          ),
-        )
+        padding: const EdgeInsets.only(left: 10,right: 200,top: 40),
+        child: Column(
+          children: const <Widget>[
+            Text('Meet new era of',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.pink
+              ),
+            ),
+            SizedBox(height: 10,),
+            Text('Natural',
+              style: TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+                color: Colors.black87
+              ),
+            ),
+            Text('Beauty',
+              style: TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+                color: Colors.black87
+              ),
+            ),
+            Text('Cosmetics',
+              style: TextStyle(
+                overflow: TextOverflow.visible,
+                fontSize: 45,
+                fontFamily: "Roboto",
+                fontWeight: FontWeight.bold,
+                color: Colors.black87
+              ),
+            ),
+          ],
+        ),
+        // child: const Text('Meet new era of',
+        // style: TextStyle(
+        //   fontSize: 20,
+        //   color: Colors.pink
+        //   ),
+        // )
       );
   }
 }
