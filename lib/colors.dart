e09c1b257ca264caad22690e255f0ea1c4a52f1f
NaveenSingh <naveensingh.mtp@gmail.com>
2021-11-04 16:08:20 +0530

    Clean Code

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,7 +1,8 @@
 import 'dart:ui';
 
 class AppColor{
-  static Color homePageTitleFirst = const Color(0xFFfbfcff);
-  static Color homePageTitleSecond = const Color(0xFFfbfcff);
-  static Color buttonColor = const Color(0xff00e2e3);
+  static Color primaryColor =  const Color(0xFF808080);
+  // static Color homePageTitleFirst = const Color(0xFFfbfcff);
+  // static Color homePageTitleSecond = const Color(0xFFfbfcff);
+  // static Color buttonColor = const Color(0xff00e2e3);
 }
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_eshop/colors.dart' as color;
 //import 'package:flutter_eshop/homepagecard.dart';
-import 'colors.dart' as color;
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -9,8 +9,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  
-
   @override
   Widget build(BuildContext context) {
     const color = Color(0xFF98d6ed);
@@ -27,105 +25,66 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         //child: HomePageBottomCard(),
         child: Container(
-          padding: const EdgeInsets.only(left: 30,top: 40),
+          padding: const EdgeInsets.only(left: 40, top: 40, right: 10),
           height: 410,
           width: MediaQuery.of(context).size.width,
           decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(30)
-          ),
+              color: Colors.white, borderRadius: BorderRadius.circular(30)),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              const Text(
-                 'make an era of',
+               const Text(
+                'Make new era of',
                   style: TextStyle(
-                  fontSize: 20,
+                  fontSize: 20, 
                   color: Colors.pink
                 ),
               ),
-              const SizedBox(height: 4,),
+              const SizedBox(height: 2),
               Container(
-                padding: const EdgeInsets.only(right: 50,top: 10),
+                padding: const EdgeInsets.only(top: 2),
                 child: Row(
-                  children:  <Widget>[
+                  children: <Widget>[
                     const Text(
-                   'Natural',
-                    style: TextStyle(
-                    fontSize: 60,
-                    color: Colors.black87,
-                    fontWeight: FontWeight.bold
-                  ),
-                  
+                      'Natural',
+                      style: TextStyle(
+                          fontSize: 60,
+                          color: Colors.black87,
+                          fontWeight: FontWeight.w800),
+                    ),
+                    Expanded(child: Container()),
+                    Container(
+                      padding: const EdgeInsets.only(right: 30, top: 10),
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: const Icon(
+                          Icons.arrow_forward,
+                          color: Colors.black87,
+                          size: 30.0,
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          primary: const Color(0xff00e2e3),
+                          shape: const CircleBorder(),
+                          padding: const EdgeInsets.all(24),
+                        ),
+                      ),
+                    )
+                  ],
                 ),
-                Expanded(child: Container()),
-                const SizedBox(height: 10,),
-                ElevatedButton(
-                     onPressed: () {},
-                     child: const Icon(
-                      Icons.arrow_forward,
-                      color: Colors.black87,
-                      size: 30.0,
-                    ),
-                      style: ElevatedButton.styleFrom(
-                      primary: const Color(0xff00e2e3),
-                      shape: const CircleBorder(),
-                      padding: const EdgeInsets.all(24),
-                    ),
-                  )
-                // Container(
-                //   //padding: const EdgeInsets.only(top: 20),
-                //   child: ElevatedButton(
-                //      onPressed: () {},
-                //       child: const Icon(
-                //         Icons.arrow_forward,
-                //         color: Colors.black87,
-                //         size: 30.0,
-                //       ),
-                //       style: ElevatedButton.styleFrom(
-                //         primary: const Color(0xff00e2e3),
-                //         shape: const CircleBorder(),
-                //         padding: const EdgeInsets.all(24),
-                //       ),
-                //   ),
-                // )
-                // Expanded(child: Container(
-                //   padding: const EdgeInsets.only(top: 40),
-                //   child: ElevatedButton(
-                //        onPressed: () {},
-                //        child: const Icon(
-                //         Icons.arrow_forward,
-                //         color: Colors.black87,
-                //         size: 30.0,
-                //       ),
-                //         style: ElevatedButton.styleFrom(
-                //         primary: const Color(0xff00e2e3),
-                //         shape: const CircleBorder(),
-                //         padding: const EdgeInsets.all(24),
-                //       ),
-                //     )
-                //   // const Text('hell'),
-                // )
-                // ),
-                
-                ],
-            ),
               ),
               const Text(
-                 'Beauty',
-                  style: TextStyle(
-                  fontSize: 60,
-                  color: Colors.black87,
-                  fontWeight: FontWeight.bold
-                ),
+                'Beauty',
+                style: TextStyle(
+                    fontSize: 60,
+                    color: Colors.black87,
+                    fontWeight: FontWeight.w800),
               ),
-               const Text(
-                 'Cosmetics',
-                  style: TextStyle(
-                  fontSize: 60,
-                  color: Colors.black87,
-                  fontWeight: FontWeight.bold
-                ),
+              const Text(
+                'Cosmetics',
+                style: TextStyle(
+                    fontSize: 60,
+                    color: Colors.black87,
+                    fontWeight: FontWeight.w800),
               )
             ],
           ),
