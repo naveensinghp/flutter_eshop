111be85f2d3870e6938107b41e373a3709fa6caa
NaveenSingh <naveensingh.mtp@gmail.com>
2021-11-26 21:03:44 +0530

    no message

--- lib/secondpage.dart
+++ lib/secondpage.dart
@@ -170,44 +170,45 @@ List productData = [];
                   int b = 2 * i + 1;
                   return Row(
                     children: <Widget>[
-                     Container(
-                       height: 200,
-                       width: (MediaQuery.of(context).size.width-90)/2,
-                       margin: const EdgeInsets.only(left: 10,bottom: 30),
-                       padding: const EdgeInsets.only(bottom: 10),
-                       decoration: BoxDecoration(
-                         color: Colors.white,
-                         borderRadius: BorderRadius.circular(17),
-                         image: DecorationImage(
-                            image: AssetImage(
-                              productData[a]['img']
-                            )
-                         ),
-                         boxShadow: [
-                           BoxShadow (
-                             blurRadius: 3,
-                             offset: const Offset(3,3),
-                             color: Colors.black.withOpacity(0.1)
-                            ),
-                            BoxShadow (
-                             blurRadius: 3,
-                             offset: const Offset(-3,-3),
-                             color: Colors.black.withOpacity(0.1)
-                            )
-                         ]
-                       ),
-                       child: Center(
-                         child: Align(
-                           alignment: Alignment.bottomCenter,
-                           child: Text(
-                          productData[a]['name'],
-                           style: const TextStyle(
-                             fontSize: 20
-                           ),
-                        ),
-                         )
-                       ),
-                     ),
+                    //  Container(
+                    //    height: 200,
+                    //    width: (MediaQuery.of(context).size.width-90)/2,
+                    //    margin: const EdgeInsets.only(left: 10,bottom: 30),
+                    //    padding: const EdgeInsets.only(bottom: 10),
+                    //    decoration: BoxDecoration(
+                    //      color: Colors.red,
+                    //      borderRadius: BorderRadius.circular(17),
+                    //      image: DecorationImage(
+                    //         image: AssetImage(
+                    //           productData[a]['img']
+                    //         )
+                    //      ),
+                    //      boxShadow: [
+                    //        BoxShadow (
+                    //          blurRadius: 3,
+                    //          offset: const Offset(3,3),
+                    //          color: Colors.black.withOpacity(0.1)
+                    //         ),
+                    //         BoxShadow (
+                    //          blurRadius: 3,
+                    //          offset: const Offset(-3,-3),
+                    //          color: Colors.black.withOpacity(0.1)
+                    //         )
+                    //      ]
+                    //    ),
+                    //    child: Center(
+                    //      child: Align(
+                    //        alignment: Alignment.bottomLeft,
+                    //        child: Text(
+                    //       productData[a]['name'],
+                    //        style: const TextStyle(
+                    //          fontSize: 20
+                    //        ),
+                    //     ),
+                    //      )
+                    //    ),
+                    //  ),
+                    
                      //second Flag
                      Container(
                        height: 200,
