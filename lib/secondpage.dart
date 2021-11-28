053e1e26fc2905a69c5ed09cc2475bb99622f01e
NaveenSingh <naveensingh.mtp@gmail.com>
2021-11-28 20:08:09 +0530

    Product Code Separated

--- lib/secondpage.dart
+++ lib/secondpage.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_eshop/product.dart';
 
 void main() => runApp(const ProductPage());
 
@@ -161,98 +161,7 @@ List productData = [];
             //   ],
             // ),
             const SizedBox(height: 30,),
-            Expanded(child: OverflowBox(
-              maxWidth: MediaQuery.of(context).size.width,
-              child: ListView.builder(
-                itemCount: (productData.length.toDouble()/2).toInt(),
-                itemBuilder: (_,i){
-                  int a = 2 * i;
-                  int b = 2 * i + 1;
-                  return Row(
-                    children: <Widget>[
-                    //  Container(
-                    //    height: 200,
-                    //    width: (MediaQuery.of(context).size.width-90)/2,
-                    //    margin: const EdgeInsets.only(left: 10,bottom: 30),
-                    //    padding: const EdgeInsets.only(bottom: 10),
-                    //    decoration: BoxDecoration(
-                    //      color: Colors.red,
-                    //      borderRadius: BorderRadius.circular(17),
-                    //      image: DecorationImage(
-                    //         image: AssetImage(
-                    //           productData[a]['img']
-                    //         )
-                    //      ),
-                    //      boxShadow: [
-                    //        BoxShadow (
-                    //          blurRadius: 3,
-                    //          offset: const Offset(3,3),
-                    //          color: Colors.black.withOpacity(0.1)
-                    //         ),
-                    //         BoxShadow (
-                    //          blurRadius: 3,
-                    //          offset: const Offset(-3,-3),
-                    //          color: Colors.black.withOpacity(0.1)
-                    //         )
-                    //      ]
-                    //    ),
-                    //    child: Center(
-                    //      child: Align(
-                    //        alignment: Alignment.bottomLeft,
-                    //        child: Text(
-                    //       productData[a]['name'],
-                    //        style: const TextStyle(
-                    //          fontSize: 20
-                    //        ),
-                    //     ),
-                    //      )
-                    //    ),
-                    //  ),
-                    
-                     //second Flag
-                     Container(
-                       height: 200,
-                       width: (MediaQuery.of(context).size.width-90)/2,
-                        margin: const EdgeInsets.only(left: 20,bottom: 30),
-                       padding: const EdgeInsets.only(bottom: 10),
-                       decoration: BoxDecoration(
-                         color: Colors.white,
-                         borderRadius: BorderRadius.circular(17),
-                         image: DecorationImage(
-                            image: AssetImage(
-                              productData[b]['img']
-                            )
-                         ),
-                         boxShadow: [
-                           BoxShadow (
-                             blurRadius: 3,
-                             offset: const Offset(5,5),
-                             color: Colors.red.withOpacity(0.1)
-                            ),
-                            BoxShadow (
-                             blurRadius: 3,
-                             offset: const Offset(-5,-5),
-                             color: Colors.red.withOpacity(0.1)
-                            )
-                         ]
-                       ),
-                       child: Center(
-                         child: Align(
-                           alignment: Alignment.bottomCenter,
-                           child: Text(
-                          productData[b]['name'],
-                           style: const TextStyle(
-                             fontSize: 20
-                           ),
-                        ),
-                         )
-                       ),
-                     ),
-                    ],
-                  );
-                }),
-            )
-            )
+            const ProductData()
           ],
         ),
         //_middleSection
