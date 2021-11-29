6db43e84c0ab6d31f5eecccbefcb880100b612c9
NaveenSingh <naveensingh.mtp@gmail.com>
2021-11-29 19:38:14 +0530

    gge

--- lib/testwidget.dart
+++ lib/testwidget.dart
@@ -5,24 +5,43 @@ class TestWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Positioned(
-     bottom: 10,
-      
-      child: Container(
-        height: 200,
-        width: 200,
-        decoration: BoxDecoration(
-        //color: Colors.white,
-        border: Border.all(),
-        borderRadius: BorderRadius.circular(18),
-          image: const DecorationImage(
-            fit: BoxFit.fitHeight,
-            image:AssetImage("assets/image/palette.jpeg")
-          )
-        ),
-        
-      ),
+    return Stack(
+      children: [
+        Positioned(
+          child: Container(
+            height: 200,
+            width: 200,
+            decoration: BoxDecoration(
+         color: Colors.red,
+         border: Border.all(),
+         borderRadius: BorderRadius.circular(18),
+           image: const DecorationImage(
+             image:AssetImage("assets/image/palette.jpeg")
+           )
+           ),
+          ),
+        )
+      ],
     );
   }
 }
 
+
+
+// Positioned(
+//     left: 15,
+      
+//       child: Container(
+//         height: 200,
+//         width: 200,
+//         decoration: BoxDecoration(
+//         //color: Colors.white,
+//         border: Border.all(),
+//         borderRadius: BorderRadius.circular(18),
+//           image: const DecorationImage(
+//             image:AssetImage("assets/image/palette.jpeg")
+//           )
+//         ),
+        
+//       ),
+//     );
