222b2a04897b64a8beda8ab92de65a7fbda39508
NaveenSingh <naveensingh.mtp@gmail.com>
2021-12-01 00:39:36 +0530

    no message

--- lib/testwidget.dart
+++ lib/testwidget.dart
@@ -6,21 +6,52 @@ class TestWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Stack(
-      children: [
-        Positioned(
-          child: Container(
-            height: 200,
+      children: <Widget>[
+        Align(
+          alignment: Alignment.topCenter,
+          child: Padding(
+            padding: EdgeInsets.only(top: 50,right: 20),
+             child: Container(
             width: 200,
+            height: 200,
             decoration: BoxDecoration(
-         color: Colors.red,
-         border: Border.all(),
-         borderRadius: BorderRadius.circular(18),
-           image: const DecorationImage(
-             image:AssetImage("assets/image/palette.jpeg")
-           )
-           ),
+              color: Colors.white,
+               border: Border.all(),
+               borderRadius: BorderRadius.circular(18),
+            ),
+            child: Container(
+              padding: const EdgeInsets.only(left: 12),
+              child: Column(
+                children: const <Widget>[
+                  Text(
+                    'Powder',
+                    style: TextStyle(
+                      fontSize: 10,
+                      fontWeight: FontWeight.bold
+                  ),
+                  ),
+                  
+                ],
+              ),
+            ),
           ),
-        )
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+           width: 100,
+           height: 100,
+           decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(),
+              image:  const DecorationImage(
+                image: AssetImage("assets/image/palette.jpeg")
+              )
+           ),
+           
+        ),
+        ),
       ],
     );
   }
@@ -28,20 +59,21 @@ class TestWidget extends StatelessWidget {
 
 
 
-// Positioned(
-//     left: 15,
-      
-//       child: Container(
-//         height: 200,
-//         width: 200,
-//         decoration: BoxDecoration(
-//         //color: Colors.white,
-//         border: Border.all(),
-//         borderRadius: BorderRadius.circular(18),
-//           image: const DecorationImage(
-//             image:AssetImage("assets/image/palette.jpeg")
-//           )
-//         ),
-        
-//       ),
+// return Stack(
+//       children: [
+//         Positioned(
+//           child: Container(
+//             height: 200,
+//             width: 200,
+//             decoration: BoxDecoration(
+//          color: Colors.red,
+//          border: Border.all(),
+//          borderRadius: BorderRadius.circular(18),
+//            image: const DecorationImage(
+//              image:AssetImage("assets/image/palette.jpeg")
+//            )
+//            ),
+//           ),
+//         )
+//       ],
 //     );
