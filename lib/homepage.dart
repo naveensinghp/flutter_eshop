7d30dc87cb4c14c36b2a53916c8e2689c6d80c00
NaveenSingh <naveensingh.mtp@gmail.com>
2021-12-30 00:57:38 +0530

    homepage alignment fixed

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -26,7 +26,8 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         //child: HomePageBottomCard(),
         child: Container(
-          padding: const EdgeInsets.only(left: 30, top: 40, right: 10),
+          //padding: const EdgeInsets.only(left: 30, top: 40, right: 10),
+          padding: const EdgeInsets.symmetric(horizontal: 25,vertical: 60),
           height: 410,
           width: MediaQuery.of(context).size.width,
           decoration: BoxDecoration(
@@ -74,7 +75,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   roundedButton(),
                 ],
               ),
-
+              const SizedBox(height: 10,),
               Row(
                 children: const [
                   Text(
@@ -87,77 +88,6 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                 ],
               )
-              // Row(
-              //   children: [
-              //     const Expanded(
-              //         flex: 4,
-              //         child:  HomeText(),
-              //     ),
-              //     Expanded(
-              //         flex: 2,
-              //         child: roundedButton()),
-              //   ],
-              // ),
-              // Column(
-              //   children: const [
-              //     Text(
-              //       'Beauty',
-              //       style: TextStyle(
-              //           fontSize: 44,
-              //           color: Colors.black87,
-              //           fontWeight: FontWeight.w800)
-              //       ,
-              //     ),
-              //   ],
-              // )
-              // Container(
-              //   padding: const EdgeInsets.only(top: 2),
-              //   child: Row(
-              //     children: <Widget>[
-              //       const Text(
-              //         'Natural',
-              //         style: TextStyle(
-              //             fontSize: 60,
-              //             color: Colors.black87,
-              //             fontWeight: FontWeight.w800),
-              //       ),
-              //       const Spacer(flex: 2,),
-              //       Container(
-              //         padding: const EdgeInsets.only(right: 20,top: 25),
-              //         child: roundedButton(),
-              //         // child: ElevatedButton(
-              //         //   onPressed: () {
-              //         //     _navigateToNextScreen(context);
-              //         //   },
-              //         //   child: const Icon(
-              //         //     Icons.arrow_forward,
-              //         //     color: Colors.black87,
-              //         //     size: 30.0,
-              //         //   ),
-              //         //   style: ElevatedButton.styleFrom(
-              //         //     primary: const Color(0xff00e2e3),
-              //         //     shape: const CircleBorder(),
-              //         //     padding: const EdgeInsets.all(24),
-              //         //   ),
-              //         // ),
-              //       )
-              //     ],
-              //   ),
-              // ),
-              // const Text(
-              //   'Beauty',
-              //   style: TextStyle(
-              //       fontSize: 60,
-              //       color: Colors.black87,
-              //       fontWeight: FontWeight.w800),
-              // ),
-              // const Text(
-              //   'Cosmetics',
-              //   style: TextStyle(
-              //       fontSize: 60,
-              //       color: Colors.black87,
-              //       fontWeight: FontWeight.w800),
-              // )
             ],
           ),
         ),
